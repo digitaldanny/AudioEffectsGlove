b470f32698b318b0d129ab7ab8f454476c3638ac
Daniel Hamilton <d.hamilton@ufl.edu>
2020-08-16 02:28:07 -0400

    Issue #1: Build switch name fixed

--- build_switches.h
+++ build_switches.h
@@ -1,6 +1,6 @@
 #ifndef _BUILD_SWITCHES
 #define _BUILD_SWITCHES
 
-#define TARGET_HW_RASBERRYPI    1   // Issue #1: Enables firmware that will only run on the prototype (Raspberry Pi).
+#define TARGET_HW_RASPBERRYPI    1   // Issue #1: Enables firmware that will only run on the prototype (Raspberry Pi).
 
 #endif
