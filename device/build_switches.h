a04afcdad840b8d097589d250e3b5167a0728659
Daniel Hamilton <d.hamilton@ufl.edu>
2020-09-20 18:58:02 -0400

    Updated build switches for testing on RPi

--- device/build_switches.h
+++ device/build_switches.h
@@ -6,8 +6,8 @@
  * TARGET HARDWARE
  * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
 */
-#define TARGET_HW_RASPBERRYPI    0   // Issue #1: Enables firmware that will only run on the prototype (Raspberry Pi).
-#define TARGET_HW_DESKTOP        1   // Issue #9: Enables dummy responses from sensors for Desktop development.
+#define TARGET_HW_RASPBERRYPI    1   // Issue #1: Enables firmware that will only run on the prototype (Raspberry Pi).
+#define TARGET_HW_DESKTOP        0   // Issue #9: Enables dummy responses from sensors for Desktop development.
 #define TARGET_HW_PYTHON_CAPABLE (1 & (TARGET_HW_RASPBERRYPI | TARGET_HW_DESKTOP)) // Determines if the selected target hardware can make calls to Python.
 
 /*
@@ -15,8 +15,8 @@
  * ENTRY POINTS (Main programs / Unit tests)
  * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
 */
-#define ENABLE_UNIT_TEST_WIRELESS_API   1   // Issue #8: Enables running the unit test for the wireless API.
-#define ENABLE_UNIT_TEST_NXP_API        0   // Issue #2: Enables unit test for the gyro/accel/mag sensor pack reading.
+#define ENABLE_UNIT_TEST_WIRELESS_API   0   // Issue #8: Enables running the unit test for the wireless API.
+#define ENABLE_UNIT_TEST_NXP_API        1   // Issue #2: Enables unit test for the gyro/accel/mag sensor pack reading.
 
 /*
  * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
