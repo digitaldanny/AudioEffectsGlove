4a3744d66d5f04d3d673fc309ccfe569b1d2a062
sweatpantsdanny <d.hamilton@ufl.edu>
2021-03-28 04:44:46 -0400

    Revert "#47: Switching to simpler library"
    This reverts commit a124f16900da01e9fb1e9a8fcd3c464f12d5deae.

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -23,6 +23,46 @@ int main()
    //lcd_loop();
 
 #if ENABLE_UNIT_TEST_MPU6050_SENSORDATA
+    int ret;
+    unsigned int c = 0; //cumulative number of successful MPU/DMP reads
+    unsigned int np = 0; //cumulative number of MPU/DMP reads that brought no packet back
+    unsigned int err_c = 0; //cumulative number of MPU/DMP reads that brought corrupted packet
+    unsigned int err_o = 0; //cumulative number of MPU/DMP reads that had overflow bit set
+
+    // index 0 stores Yaw-Pitch-Roll, index 1 stores raw gyro data
+    float yaw[2];
+    float pitch[2];
+    float roll[2];
+
+    I2c::init();
+
+    ret = mympu_open(200);
+
+    while(1)
+    {
+        switch (ret) {
+        case 0: c++; break;
+        case 1: np++; break;
+        case 2: err_o++; break;
+        case 3: err_c++; break;
+        default:
+            break;
+        }
+
+        if (!(c%25))
+        {
+            yaw[0] = mympu.ypr[0];
+            yaw[1] = mympu.gyro[0];
+
+            pitch[0] = mympu.ypr[1];
+            pitch[1] = mympu.gyro[1];
+
+            roll[0] = mympu.ypr[2];
+            roll[1] = mympu.gyro[2];
+
+            roll[0] = -1.0f; // breakpoint here
+        }
+    }
 
 #endif // ENABLE_UNIT_TEST_MPU6050_SENSORDATA
 
