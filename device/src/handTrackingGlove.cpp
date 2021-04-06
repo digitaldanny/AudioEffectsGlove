a6478b828510ea2edb43a0832fcfd02e382aee30
sweatpantsdanny <d.hamilton@ufl.edu>
2021-04-06 18:30:30 -0400

    #47: Adding in raw sensor reading code to main program

--- device/src/handTrackingGlove.cpp
+++ device/src/handTrackingGlove.cpp
@@ -5,6 +5,7 @@
 #include "data_packet_protocol.h"
 #include "hc05_api.h"
 #include "flex_sensors_api.h"
+#include "i2c_if.h"
 #include "lcd_graphics.h"
 
 /*
@@ -17,6 +18,41 @@
 #define LCD_COL_BLUETOOTH_STATUS    0
 
 #define BLUETOOTH_TIMEOUT_COUNT     500 // Iteration count between data update to ack is expected to be around 10-15.
+
+/*
+ * +=====+=====+=====+=====+=====+=====+=====+==5===+=====+=====+=====+=====+
+ * DEFINES
+ * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
+*/
+
+// Configuration register indices
+#define MPU6050_NUM_CONFIGS         3
+#define MPU6050_PWR_MGMT_1          0
+#define MPU6050_GYRO_CONFIG         1
+#define MPU6050_ACCEL_CONFIG        2
+
+// Other
+#define MPU6050_DEV_ADDR            0x68
+#define MPU6050_SENSOR_ADDR_START   0x3B    // XACCEL_H is the first address to read from to capture all sensor values
+#define MPU6050_SENSOR_BURST_LEN    14      // 6 regs Accel X-Z H/L, 2 regs Temp H/L, 6 regs Gyro X-Z H/L
+
+/*
+ * +=====+=====+=====+=====+=====+=====+=====+==5===+=====+=====+=====+=====+
+ * GLOBALS
+ * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
+*/
+
+uint8_t sensorBuffer[MPU6050_SENSOR_BURST_LEN];
+int16_t accelBuffer[3];
+int16_t gyroBuffer[3];
+
+// NOTE - Only append to the end of this array. **ORDER MATTERS**
+// Each config contains the following - Register addr, register value
+uint8_t mpu6050Configs[MPU6050_NUM_CONFIGS][2] = {
+    /* MPU6050_PWR_MGMT_1   */ {0x68, 0x00},
+    /* MPU6050_GYRO_CONFIG  */ {0x1B, 0x08}, // +- 500 deg/s
+    /* MPU6050_ACCEL_CONFIG */ {0x1C, 0x08} // +- 4g
+};
 
 /*
  * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
@@ -110,6 +146,18 @@ int handTrackingGlove()
     // Slave will be ready for update as soon as master and slave connect
     state.isSlaveReadyForUpdate = true;
 
+    // Initialize I2C module and configure MPU6050 registers
+    I2c::init();
+
+    // Write configuration register values
+    for (int iConfig = 0; iConfig < MPU6050_NUM_CONFIGS; iConfig++)
+    {
+        I2c::write(MPU6050_DEV_ADDR, mpu6050Configs[iConfig][0], 1, &mpu6050Configs[iConfig][1]);
+        delayMs(1);
+    }
+
+    delayMs(10);
+
     // Main program loop
     while(1)
     {
@@ -118,6 +166,28 @@ int handTrackingGlove()
 
         // Capture and compress ADC readings for all flex sensors
         updateFlexSensorReadings();
+
+        /////// UDPATE ACCELEROMETER AND GYROSCOPE DATA HERE
+        if (!I2c::read(MPU6050_DEV_ADDR, MPU6050_SENSOR_ADDR_START, MPU6050_SENSOR_BURST_LEN, sensorBuffer))
+        {
+            while (1); // read failed, trap CPU for debugging
+        }
+
+        accelBuffer[0] = (sensorBuffer[0] << 8) | (sensorBuffer[1]);
+        accelBuffer[1] = (sensorBuffer[2] << 8) | (sensorBuffer[3]);
+        accelBuffer[2] = (sensorBuffer[4] << 8) | (sensorBuffer[5]);
+        // temp value is stored in indices 6-7
+        gyroBuffer[0] = (sensorBuffer[8] << 8) | (sensorBuffer[9]);
+        gyroBuffer[1] = (sensorBuffer[10] << 8) | (sensorBuffer[11]);
+        gyroBuffer[2] = (sensorBuffer[12] << 8) | (sensorBuffer[13]);
+
+        for (uint8_t f = 0; f < 3; f++)
+        {
+            memset(state.lcdMsg, 0, LCD_MAX_CHARS_PER_LINE);
+            sprintf(state.lcdMsg, "A%u: %u", f, accelBuffer[f]);
+            LcdGfx::drawString(0, f + 1, state.lcdMsg, LCD_MAX_CHARS_PER_LINE);
+        }
+        /////// UDPATE ACCELEROMETER AND GYROSCOPE DATA HERE
 
         if (state.isSlaveConnected)
         {
@@ -193,10 +263,10 @@ bool updateFlexSensorReadings()
     {
         state.packet.flexSensors[f] = (unsigned char)FlexSensors::GetJointsData(f);
 
-        // Update LCD with flex sensor readings
-        memset(state.lcdMsg, 0, LCD_MAX_CHARS_PER_LINE);
-        sprintf(state.lcdMsg, "F%u: %u", f, state.packet.flexSensors[f]);
-        LcdGfx::drawString(0, f + 1, state.lcdMsg, LCD_MAX_CHARS_PER_LINE);
+        // // Update LCD with flex sensor readings
+        // memset(state.lcdMsg, 0, LCD_MAX_CHARS_PER_LINE);
+        // sprintf(state.lcdMsg, "F%u: %u", f, state.packet.flexSensors[f]);
+        // LcdGfx::drawString(0, f + 1, state.lcdMsg, LCD_MAX_CHARS_PER_LINE);
     }
     return true;
 }
